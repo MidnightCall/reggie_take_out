7f6c34150748c5b20a6a897f0c3ada8caab4c352
kojikoji <1310402980@qq.com>
2023-04-04 15:00:22 +0800

    配置redis

---
+++ src/main/java/com/kojikoji/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.kojikoji.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @ClassName RedisConfig
+ * @Description
+ * @Author kojikoji 1310402980@qq.com
+ * @Date 2023/4/4 14:42
+ * @Version
+ */
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
